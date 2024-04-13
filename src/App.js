32bee188f34848aa5dca1921aebf37d7baed5f9d
Safal Mehrotra <safalmehrotra1216@gmail.com>
2024-04-13 17:14:48 +0530

    photo added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Hero from './Hero';
 import Navbar from './navbar';
+
 
 function App() {
   return (
     <>
       <header className="App-header">
         <Navbar />
+        <Hero />
       </header>
     </>
   );
---
+++ src/Hero.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default function Hero() {
+    return (
+        <section>
+            <img src="./photo-grid.png" alt="logo" className='nav--logo'/>
+        </section>
+    )
+}
