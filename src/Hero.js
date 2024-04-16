abc99149c108c092356732250d46bd7f0e978198
Safal Mehrotra <safalmehrotra1216@gmail.com>
2024-04-16 18:27:09 +0530

    data file added

--- src/Hero.js
+++ src/Hero.js
@@ -5,7 +5,10 @@ import ReactDOM from 'react-dom/client';
 export default function Hero() {
     return (
         <section>
-            <img src="./photo-grid.png" alt="logo" className='nav--logo'/>
+            <img src="./photo-grid.png" alt="logo" className='hero--photo'/>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
         </section>
     )
 }
